eb4748cfe1871f43d2f453f681858d2c43d22a22
Aaron <aaronmsegal@n3-80-212.dhcp.drexel.edu>
2017-05-29 17:13:49 -0400

    lsmr clean

--- lsmr.py
+++ lsmr.py
@@ -1,66 +1,27 @@
-##
-
 import math
 import numpy
 
-state = numpy.array([0,0])
-prices = numpy.array([.5, .5])
-
 ## Cost function
-def C(S):
+def C(state):
     sum = 0
-    for s in S:
-        sum += math.exp(s)
+    for s in state:
+        sum += math.exp(state)
     return math.log(sum) # Natural Log
 
-def getTotalPrice(S):
-    return C(S) - C(state)
+def getTotalPrice(currentState, bid):
+    return C(bid) - C(currentState)
 
-def doTrade(S):
-    global state
-    global prices
-
-    state = state + S
+def doTrade(currentState, prices, bid):
+    newState = currentState + bid
     sum = 0
-    for o in state:
-        sum += math.exp(o)
-    
-    for i in xrange(0, S.size):
-        prices[i] = math.exp(state[i]) / sum
+    for outcome in newState:
+        sum += math.exp(outcome)
 
-def printState():
-    global state
-    global prices
-    print "state = {}\nprices = {}".format(state, prices)
+    for i in xrange(0, bid.size):
+        prices[i] = math.exp(newState[i]) / sum
 
-def getPrices():
-    global prices
-    return prices
+    return newState, prices
 
-def makeTrade(userId, bid):
-    doTrade(numpy.array(map(int, bid.split(','))))
-    return ""
-
-'''
-newstate = numpy.array([1,0])
-print getTotalPrice(newstate)
-printState()
-makeTrade(newstate)
-
-newstate = numpy.array([2,0])
-print getTotalPrice(newstate)
-printState()
-makeTrade(newstate)
-
-newstate = numpy.array([0,1])
-print getTotalPrice(newstate)
-printState()
-makeTrade(newstate)
-
-newstate = numpy.array([-1, 0])
-print getTotalPrice(newstate)
-printState()
-makeTrade(newstate)
-
-printState()
-'''
+# def makeTrade(userId, bid):
+#     doTrade(numpy.array(map(int, bid.split(','))))
+#     return
