1757a148801fa5daa4aa73a79082d2e11d739e02
Alex Palmatier <asp78@drexel.edu>
2017-06-08 12:53:54 -0400

    something

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, send_file
 from auction import auction
 from htmlMaker import helpPage
 from htmlMaker import auctionPage
+from htmlMaker import netWorth
 
 import lsmr
 import traceback
@@ -24,7 +25,15 @@ def getPrices():
     try:
         return a.getPrices()
     except Exception as e:
-        print e
+        print traceback.print_exc()
+        return "An error occurred."
+
+@app.route('/getNetWorth/<outcome>/')
+def getNetWorth(outcome):
+    try:
+        return netWorth(a, outcome)
+    except Exception as e:
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/getPriceLogs/', methods=['GET'])
@@ -32,7 +41,7 @@ def getPriceLogs():
     try:
         return send_file('prices.txt')
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/getStateLogs/', methods=['GET'])
@@ -40,7 +49,7 @@ def getStateLogs():
     try:
         return send_file('states.txt')
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/getTradeLogs/', methods=['GET'])
@@ -48,7 +57,7 @@ def getTradeLogs():
     try:
         return send_file('trades.txt')
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/getCost/<bid>/', methods=['GET'])
@@ -56,7 +65,7 @@ def getCost(bid):
     try:
         return str(a.getCost(bid))
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/makeTrade/<userId>/<bid>/', methods=['GET'])
@@ -64,7 +73,7 @@ def makeTrade(userId, bid):
     try:
         return a.makeTrade(userId, bid)
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/addUser/<userId>/', methods=['GET'])
@@ -72,7 +81,7 @@ def addUser(userId):
     try:
         return a.addUser(userId)
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/status/<userId>/', methods=['GET'])
@@ -80,7 +89,6 @@ def getStatus(userId):
     try:
         return a.getStatus(userId)
     except Exception as e:
-        print e
         print traceback.print_exc()
         return "An error occurred."
 
@@ -89,7 +97,7 @@ def closeRegistration():
     try:
         return a.closeRegistration()
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/closeAuction/', methods=['GET'])
@@ -97,7 +105,7 @@ def closeAuction():
     try:
         return a.closeAuction()
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/winningOutcome/<index>/', methods=['GET'])
@@ -105,7 +113,7 @@ def winningOutcome(index):
     try:
         return a.winningOutcome(index)
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/auctionResults/', methods=['GET'])
@@ -113,7 +121,7 @@ def auctionResults():
     try:
         return a.auctionResults()
     except Exception as e:
-        print e
+        print traceback.print_exc()
         return "An error occurred."
 
 @app.route('/help/', methods=['GET'])
