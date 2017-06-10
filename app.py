dc876d05ddacfb59d030f207d108d919acb83c40
aaronmsegal <aaronmsegal@gmail.com>
2017-06-09 20:32:38 -0400

    graph in

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', debug=False, threaded=True)
     except Exception as e:
-        print "{}".format(e)
+        print traceback.print_exc()
